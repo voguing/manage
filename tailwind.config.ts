7b7052dc742dab7a61bc92e143233aae0ebdbb06
xleojs <xleo.js@gmail.com>
2024-05-31 23:04:26 +0800

    Initial commit from Create Next App

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import type { Config } from "tailwindcss"
 
 const config = {
@@ -78,3 +79,25 @@ const config = {
 } satisfies Config
 
 export default config
+=======
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
+>>>>>>> fd9bf1c (Initial commit from Create Next App)
