3f7a98252921f4533c73c1ca33e0bf23989ac310
xleojs <xleo.js@gmail.com>
2024-07-04 00:16:30 +0800

    chore: Remove unused GRAPHQL_SERVER environment variable

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  async rewrites() {
-    return [
-      {
-        source: "/graphql",
-        destination: process.env.GRAPHQL_SERVER,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
